4c794ded225fd26452b74a2178166692994e7ded
Emi Hayakawa <52098983+hayamin1111@users.noreply.github.com>
2026-06-21 22:18:05 +0900

    feat: API連携

--- src/components/BookSearch/index.tsx
+++ src/components/BookSearch/index.tsx
@@ -7,33 +7,41 @@ import BookSearchResults from "@/components/BookSearch/BookSearchResults";
 
 const mockBooks: Book[] = [
   {
-    id: '1',
-    title: '仮のタイトル',
-    authors: ['仮の著者'],
+    id: "1",
+    title: "仮のタイトル",
+    authors: ["仮の著者"],
   },
   {
-    id: '2',
-    title: '仮のタイトル2',
-    authors: ['仮の著者2', '仮の著者2-2'],
+    id: "2",
+    title: "仮のタイトル2",
+    authors: ["仮の著者2", "仮の著者2-2"],
   },
 ];
 
-export default function BookSearch () {
+export default function BookSearch() {
   /**
    * 状態管理
    */
   const [books, setBooks] = useState<Book[]>([]); //書籍情報
 
-  const handleSearch = (searchWord: string) => {
+  const handleSearch = async (searchWord: string) => {
     if (searchWord === "") {
       setBooks([]);
       return;
     }
 
-    const results = mockBooks.filter(book => (book.title.includes(searchWord))); //タイトル検索
-    
+    const query = `${searchWord} subject:science+fiction`; // 検索ワードにSFを追加する
+    const encodedQuery = encodeURIComponent(query); // 文字列を安全にエンコード
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${encodedQuery}&printType=books&maxResults=10`;
+
+    const response = await fetch(url);
+    if (!response.ok) return;
+    console.log(response);
+
+    const results = mockBooks.filter((book) => book.title.includes(searchWord)); //タイトル検索
+
     setBooks(results);
-  }
+  };
 
   return (
     <>
@@ -41,9 +49,8 @@ export default function BookSearch () {
         <BookSearchForm onSearch={handleSearch} />
       </div>
       <div>
-        <BookSearchResults books={books}/>
+        <BookSearchResults books={books} />
       </div>
     </>
-  )
-
+  );
 }
